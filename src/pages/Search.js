e392031fe910256cf0790de49ca3d71b63fbef19
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-12-18 03:23:10 -0500

    added context and API call

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,16 +1,32 @@
-import React from "react";
-//import API from "../utils/API";
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
+import EmployeeContext from "../utils/EmployeeContext";
 import Hero from "../components/Hero";
 import Table from "../components/Table";
 
 function Search() {
+  const [employeeState, setEmployeeState] = useState([]);
+
+  useEffect(() => {
+    API.getUsers()
+      .then((res) => {
+        if (res.data.length === 0) {
+          throw new Error("Empty Set Returned");
+        }
+        setEmployeeState(res.data.results);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  });
   return (
     <div>
       <Hero>
         <h1 className="display-4">Employee Directory</h1>
       </Hero>
-
-      <Table />
+      <EmployeeContext.Provider value={employeeState}>
+        <Table />
+      </EmployeeContext.Provider>
     </div>
   );
 }
