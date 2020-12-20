e8ca73ef6461821df17423cbb566ff72021f3f5b
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-12-20 03:25:16 -0500

    states, effect etc.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,32 +1,54 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import API from "../utils/API";
-import EmployeeContext from "../utils/EmployeeContext";
+//import EmployeeContext from "../utils/EmployeeContext";
 import Hero from "../components/Hero";
 import Table from "../components/Table";
 
+//create three states: employee, filtered, searchString
+
+//hansle input change
+//update seachString
+//filter to filtered
+
 function Search() {
-  const [employeeState, setEmployeeState] = useState([]);
+  const [employees, setEmployees] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [searchString, setSearchString] = useState([]);
 
   useEffect(() => {
+    if (employees.length === 0) {
+      loadEmployees();
+    } else if (employees.length !== 0) {
+      filterEmployees();
+    }
+  }, [employees, filtered, searchString]);
+
+  function loadEmployees() {
+    console.log("loadEmployees()");
     API.getUsers()
       .then((res) => {
         if (res.data.length === 0) {
           throw new Error("Empty Set Returned");
         }
-        setEmployeeState(res.data.results);
+        const results = res.data.results;
+        setEmployees(results);
       })
       .catch((err) => {
         throw err;
       });
-  });
+  }
+
+  function filterEmployees() {
+    setFiltered(employees);
+  }
+
   return (
     <div>
       <Hero>
         <h1 className="display-4">Employee Directory</h1>
       </Hero>
-      <EmployeeContext.Provider value={employeeState}>
-        <Table />
-      </EmployeeContext.Provider>
+      <Table employeeList={filtered} />
     </div>
   );
 }
