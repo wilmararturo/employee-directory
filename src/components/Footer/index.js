d7c5cde70f0822ce2a3be1a603f3dc4f601e7966
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-12-20 03:22:52 -0500

    adjusted styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,11 +3,13 @@ import "./style.css";
 
 function Footer() {
   return (
-    <footer className="footer bg-dark text-white m-auto p-3 col-sm-12">
-      <div className="col-lg-12 text-center">
-        <span className="text-muted"> Employee Directory &copy; 2020 </span>
-      </div>
-    </footer>
+    <div className="footer">
+      <footer className="footer bg-dark text-white m-auto p-3 col-sm-12">
+        <div className="col-lg-12 text-center">
+          <span className="text-muted"> Employee Directory &copy; 2020 </span>
+        </div>
+      </footer>
+    </div>
   );
 }
 
