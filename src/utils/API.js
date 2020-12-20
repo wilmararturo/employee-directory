e23903fe119e80c7d5dfc68385cc906c73ba2c1e
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-12-20 03:21:48 -0500

    updated count

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
   getUsers: function () {
-    return axios.get("https://randomuser.me/api/?results=250&nat=us");
+    return axios.get("https://randomuser.me/api/?results=200&nat=us");
   },
 };
